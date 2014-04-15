9d40c771bc10611e78bd987d30c76a6bf2c39b2d
oliroberts <o.roberts@leeds.ac.uk>
2014-04-15 11:07:41 +0100

    Functionality updates
    Find/append snippets. Show/hide snippets.

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,3 +1,4 @@
+// Temporary Navigation
 $("h2").each(function(index, value) {
     if ($(this).children("a").attr("href") != undefined) {
         $("#main_navigation").append("<li><a href='" + $(this).children("a").attr("href") + "'>" + $(this).children("a").attr("href") + "</li>");
@@ -6,6 +7,7 @@ $("h2").each(function(index, value) {
 
 // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 
+//Mobile navigation
 $(function() {
     $('#main_navigation').slicknav({
         prependTo: '#mobileNav'
@@ -14,6 +16,26 @@ $(function() {
 
 // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 
+//Append each HTML sippet within <pre></pre>
+$('.html-snippet').each(function() {
+    var thisText = $(this).html();
+    $(this).append('<pre>' + $.trim(thisText) + '</pre><div class="reveal-html">Show HTML</div>');
+
+});
+
+//Show/Hide snippets
+$('.reveal-html').bind("click", function() {
+    $(this).toggleClass("show").prev("pre").toggle(200);
+});
+
+//Fix the pre formatting
+$('pre').each(function() {
+    $(this).text($(this).html());
+});
+
+// . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+
+//Simple accordion
 (function($) {
 
     var allPanels = $('.accordion > dd').hide();
@@ -31,6 +53,7 @@ $(function() {
 
 // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 
+//Smooth scroll effect
 $(function() {
     $('a[href*=#]:not([href=#])').click(function() {
         $('.active-pattern').removeClass('active-pattern');
